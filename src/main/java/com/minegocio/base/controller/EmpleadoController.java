681a901d275aacaefb56fce430cd5fada83b0bf7
fmcuevas <fmcuevas1985@outlook.com>
2021-03-17 21:08:10 -0300

    Se agrego toda la funcioalidad para Empleado, Sigue sin funcionar

--- src/main/java/com/minegocio/base/controller/EmpleadoController.java
+++ src/main/java/com/minegocio/base/controller/EmpleadoController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.minegocio.base.domain.Empleado;
+import com.minegocio.base.domain.Persona;
 import com.minegocio.base.service.BarrioService;
 import com.minegocio.base.service.CiudadService;
 import com.minegocio.base.service.EmpleadoService;
@@ -84,12 +85,11 @@ public class EmpleadoController implements IController<Empleado>{
 		
 		return "base/empleados/new";
 	}
-	
 	@PostMapping("create")
 	public String create(@ModelAttribute Empleado empleado) {
 		empleado.setActivo(true);
-		//empleado.setNombre(empleado.getNombre().toUpperCase());
-		//empleado.setGentilicio(empleado.getGentilicio().toUpperCase());
+		empleado.setNombres(empleado.getNombres().toUpperCase());
+		empleado.setApellidos(empleado.getApellidos().toUpperCase());
 		service.create(empleado);
 		return "redirect:/base/empleados";
 	}
@@ -100,9 +100,11 @@ public class EmpleadoController implements IController<Empleado>{
 	public String show(@PathVariable Long id, Model model) {
 		model.addAttribute("modulo", " "+ConfigModulosMenus.base().nombre.toUpperCase());
 		model.addAttribute("menus", ConfigModulosMenus.base().menus);
-		model.addAttribute("titulo_cuerpo","Datos País");
+		model.addAttribute("titulo_cuerpo","Datos Empleado");
+		
 		Empleado empleado = service.findById(id);
 		model.addAttribute("empleado", empleado);
+		
 		return "base/empleados/show";
 	}
 	
@@ -115,6 +117,8 @@ public class EmpleadoController implements IController<Empleado>{
 		model.addAttribute("titulo_cuerpo","Actualizar Datos");
 		
 		Empleado empleado = service.findById(id);
+		model.addAttribute("lista_ciudades", ciudadService.findAll());
+		model.addAttribute("lista_barrios", barrioService.findAll());
 		model.addAttribute("empleado", empleado);
 		
 		return "base/empleados/edit";
@@ -133,6 +137,7 @@ public class EmpleadoController implements IController<Empleado>{
 	@GetMapping("/delete/{id}")
 	public String destroy(@PathVariable Long id) {
 	    service.deleteById(id);
+	    
 	    return "redirect:/base/empleados";
 	}
 	
